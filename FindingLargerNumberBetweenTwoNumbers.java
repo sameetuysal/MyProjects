111a9aad5ddf854cd1d5667613c877f3816d2a3e
sameetuysal <sametuysal_12@hotmail.com>
2017-01-08 01:29:00 +0300

    Find the larger number between two numbers

---
+++ FindingLargerNumberBetweenTwoNumbers.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+public class FindingLargerNumberBetweenTwoNumbers {
+	
+	public static void main(String [] args)
+	{
+		int sonuc ;
+		
+		Scanner sayiAl = new Scanner (System.in);
+		
+		System.out.println("Please enter the two numbers to find that which one is bigger =>");
+		
+		int a = sayiAl.nextInt();
+		int b = sayiAl.nextInt();
+		
+		sonuc = büyüksayi(a,b);
+		
+		System.out.println("Larger number => " + sonuc);
+		
+		sayiAl.close();
+		
+	}
+
+	static int büyüksayi(int a , int b)
+	{
+		int büyüksayi ;
+		
+		if (a > b)
+			  büyüksayi = a ;
+		else if(b > a)
+			büyüksayi = b ;
+		else
+			büyüksayi = a ;
+		
+		return büyüksayi ;
+	}
+
+}
